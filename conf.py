bba376d95d5fe49ca7e3ff7360319bab3012975b
kd2718 <koryd2718@gmail.com>
2021-01-12 10:02:39 -0800

    Nikola auto commit.
    Nikola version: 8.1.2

--- conf.py
+++ conf.py
@@ -1020,7 +1020,7 @@ COMMENT_SYSTEM = "utterances"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "koryd/letsimpute2"
+COMMENT_SYSTEM_ID = "kd2718/letsimpute2"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
